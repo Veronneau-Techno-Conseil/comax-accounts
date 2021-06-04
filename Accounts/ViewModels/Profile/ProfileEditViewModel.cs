07957d0494454493cfa0c15bd3ffcd90e73ba719
MohMsh <50699848+MohMsh@users.noreply.github.com>
2021-06-04 01:49:18 -0400

    main menu + profile update  + hardcoding user accounttype in registration

---
+++ Accounts/ViewModels/Profile/ProfileEditViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunAxiom.Accounts.ViewModels.Profile
+{
+    public class ProfileEditViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public byte[] ProfilePicture { get; set; }
+    }
+}
