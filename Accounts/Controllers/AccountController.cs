9ea51190fe87d2594a62dcdc2ead96f007aec98a
Wesley Van Wyk <wesley_van_wyk@hotmail.com>
2022-02-06 21:18:04 -0500

    revert commit on test for checkin.

--- Accounts/Controllers/AccountController.cs
+++ Accounts/Controllers/AccountController.cs
@@ -43,7 +43,6 @@ namespace CommunAxiom.Accounts.Controllers
         [AllowAnonymous]
         public IActionResult Login(string returnUrl = null)
         {
-            string name = "test for checkin"
             ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
