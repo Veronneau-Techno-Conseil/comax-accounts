93f82e4254c3701f50560213095fcd88c12c921b
Wesley Van Wyk <wesley_van_wyk@hotmail.com>
2022-05-10 23:13:50 -0400

    test push

---
+++ src/Accounts/Controllers/NetworkController.cs
@@ -0,0 +1,59 @@
+﻿using CommunAxiom.Accounts.Models;
+using CommunAxiom.Accounts.ViewModels.Network;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommunAxiom.Accounts.Controllers
+{
+    public class NetworkController : Controller
+    {
+        private readonly Models.AccountsDbContext _context;
+
+        public NetworkController(AccountsDbContext context, UserManager<User> userManager)
+        {
+            this._context = context;
+
+        }
+
+        [HttpGet]
+        public IActionResult Manage()
+        {
+            var contacts = _context.Set<Models.Contact>().Include(x => x.PrimaryAccount).Include(x => x.User)
+                .Where(x => x.PrimaryAccount.UserName == User.Identity.Name)
+                .Select(x => new Contact { Id = x.Id, PrimaryAccount = x.PrimaryAccount, User = x.User, UserId = x.UserId, PrimaryAccountId = x.PrimaryAccountId, CreationStatus = x.CreationStatus, CreationStatusId = x.CreationStatusId }).ToList();
+
+            var groups = _context.Set<Models.Group>().Include(x => x.Owner)
+                .Where(x => x.Owner.UserName == User.Identity.Name)
+                .Select(x => new Models.Group { Id = x.Id, Owner = x.Owner, OwnerId = x.OwnerId }).ToList();
+
+            var contactRequests = _context.Set<Models.ContactRequest>().Include(x => x.Contact)
+                .Where( x => x.Contact.PrimaryAccount.UserName == User.Identity.Name)
+                .Select(x => new Models.ContactRequest
+                {
+                    Id = x.Id,
+                    Contact = x.Contact,
+                    ContactId = x.ContactId,
+                    ContactStatus = x.ContactStatus,
+                    ContactStatusId = x.ContactStatusId,
+                    IdProvider = x.IdProvider,
+                    IdProviderId = x.IdProviderId,
+                    Notification = x.Notification,
+                    NotificationId = x.NotificationId,
+                    DateSent = x.DateSent
+                }).ToList();
+ 
+            var model = new ManageViewModel            {
+                Contacts = contacts,
+                Groups = groups,
+                ContactRequests = contactRequests
+            };
+
+            return View(model);
+        }
+
+    }
+}
+
